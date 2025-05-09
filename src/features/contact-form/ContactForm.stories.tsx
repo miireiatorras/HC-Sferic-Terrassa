3a994c4f9a6ffdde9f436e3563c71faef690e655
Mireia Torras <mireiat.mtc@gmail.com>
2025-05-10 00:28:47 +0200

    Update ContactForm.stories.tsx

--- src/features/contact-form/ContactForm.stories.tsx
+++ src/features/contact-form/ContactForm.stories.tsx
@@ -1,24 +1,24 @@
-import type { Meta, StoryObj } from '@storybook/react';
-import {
-    ContactForm,
-    Props as ContactFormProps,
-} from '../../features/contact-form/ContactForm';
+// import type { Meta, StoryObj } from '@storybook/react';
+// import {
+//     ContactForm,
+//     Props as ContactFormProps,
+// } from '../../features/contact-form/ContactForm';
 
-const meta: Meta<typeof ContactForm> = {
-    title: 'Sferic/ContactForm',
-    component: ContactForm,
-    tags: ['autodocs'],
-    argTypes: {
-        title: { control: 'text' },
-        onSubmit: { action: 'submitted' },
-    },
-};
-export default meta;
+// const meta: Meta<typeof ContactForm> = {
+//     title: 'Sferic/ContactForm',
+//     component: ContactForm,
+//     tags: ['autodocs'],
+//     argTypes: {
+//         title: { control: 'text' },
+//         onSubmit: { action: 'submitted' },
+//     },
+// };
+// export default meta;
 
-type Story = StoryObj<typeof ContactForm>;
+// type Story = StoryObj<typeof ContactForm>;
 
-export const Default: Story = {
-    args: {
-        title: 'Title of the Form',
-    } as ContactFormProps,
-};
+// export const Default: Story = {
+//     args: {
+//         title: 'Title of the Form',
+//     } as ContactFormProps,
+// };
